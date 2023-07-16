d25ecacc5d3d5dd62f5e3683d13ee60c8632403c
Dahyeon Lee <dada4202@gmail.com>
2023-07-16 14:30:23 +0900

    Typescript필수문법 10분 정리와 설치 셋팅

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-var n = "dana";
-var a = ["dana", "sehoon"];
-var o = { name: "kim" };
-var u = "kim"; //Union type
-var t = 123;
-function f(x) {
+//이 변수엔 string(문자) type만 들어올 수 있다.
+var 스트링 = "kim";
+var 넘버 = 123;
+var 불린 = true;
+var 널 = null;
+var 언디파인트 = undefined;
+var 배열 = ["kim", "park"];
+var 오브젝트 = { name: "kim" };
+var 오브젝트2 = {}; //물음표 표시를 넣으면 name속성은 옵션이라는 뜻.
+//string, number, boolean, null, undefined, bigint, [], {} 등...
+//문자나 숫자 둘 다 가능(union타입)
+var 이름 = "kim";
+var 이상형 = 123;
+//함수도 타입지정 가능
+function 함수(x) {
     return x * 2;
 }
 var john = [123, true];
-var group = { name: "kim", age: "123" };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
+var dana = { name: "kim" };
--- index.ts
+++ index.ts
@@ -1,27 +1,31 @@
-let n :string = "dana";
-let a :string[] = ["dana","sehoon"];
-let o :{name? : string} = {name : "kim"};
+//이 변수엔 string(문자) type만 들어올 수 있다.
+let 스트링 : string = "kim";
+let 넘버 : number = 123;
+let 불린 : boolean = true;
+let 널 : null = null;
+let 언디파인트 : undefined = undefined;
+let 배열 : string[] = ["kim","park"];
+let 오브젝트 : {name : string} = {name : "kim"};
+let 오브젝트2 : {name? : string} = {}; //물음표 표시를 넣으면 name속성은 옵션이라는 뜻.
+//string, number, boolean, null, undefined, bigint, [], {} 등...
 
-let u : string | number = "kim"; //Union type
+//문자나 숫자 둘 다 가능(union타입)
+let 이름 : string | number = "kim";
 
-type Type = string | number;
-let t : Type = 123;
+//타입을 변수에 담아서 사용 가능
+//이런 경우 첫 문자는 대문자로 
+type Mytype = string | number;
+let 이상형 : Mytype = 123;
 
-function f(x : number) : number{
-    return x * 2;
+//함수도 타입지정 가능
+function 함수(x : number) : number{
+ return x*2;
 }
 
-type Member = [number, boolean]; //array에 쓸 수 있는 tuple타입
+type Member = [number,boolean];
 let john:Member = [123,true];
 
-type MemeberObject = {
-    [key:string] : string
+type Member2 = {
+    [key:string] : string,
 }
-let group:MemeberObject = {name : "kim", age : "123"};
-
-class User {
-    name : string;
-    constructor(name:string){
-        this.name = name;
-    }
-}
+let dana:Member2 = {name : "kim"};
